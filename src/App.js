021b08466aa227608e88e44729a481d5b0d56ae5
우진 <woojin9812071207@gmail.com>
2022-11-25 01:20:12 +0900

    youtube background play

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import { Component } from "react";
+import YoutubeBackground from "react-youtube-background";
+
 
 class App extends Component {
+  video_pause = () => {
+    console.log('Click happened');
+  };
   render(){
+    const vdid = 'K4OltK36GUo'
     return (
-      <div>
-        Hello world!!
-      </div>
+    <div>
+      <h1>
+      MusicQuiz
+        <YoutubeBackground
+        className="yt"
+        videoId= {vdid} // 0trXUYYeNHY
+        playerOptions={{ mute: 0, loop: 1 }}
+        >
+    </YoutubeBackground>
+      </h1>
+
+    </div>
     )
   }
 }
