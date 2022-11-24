1684050960b3ba35deb83e6f26cca278a6229160
우진 <woojin9812071207@gmail.com>
2022-11-24 21:57:38 +0900

    hello world!

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class App extends Component {
+  render(){
+    return (
+      <div>
+        Hello world!!
+      </div>
+    )
+  }
+}
+
+export default App;
