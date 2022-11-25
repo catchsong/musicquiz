7314378239119ad7c24c7d1a444d002adb2f3dad
우진 <woojin9812071207@gmail.com>
2022-11-26 08:37:28 +0900

    new func

--- src/App.js
+++ src/App.js
@@ -5,13 +5,23 @@ import videoplay from "./video_play"
 class App extends Component {
   state = {
     playing: true,
-    idx: 0
+    idx: 0,
+    text : "정답 입력",
+    res : "fail", 
   }
   render(){
     const { idx } = this.state;
     const video_url = [
-      "https://www.youtube.com/watch?v=K4OltK36GUo",
-      "https://www.youtube.com/watch?v=ZNEuWldWPD4",
+      {
+        index: 1,
+        ans : '츠나각성',
+        url: "https://www.youtube.com/watch?v=K4OltK36GUo"
+      },
+      {
+        index: 2,
+        ans : 'antifragile',
+        url: "https://www.youtube.com/watch?v=ZNEuWldWPD4"
+      },
     ]
 
     const nextVideo = () => {
@@ -21,6 +31,19 @@ class App extends Component {
         this.setState({ idx: 0 })
       }
     };
+    const onTextChange = (e) => {
+      this.setState({
+        text: e.target.value
+      });
+      if (e.target.value == video_url[idx].ans)
+      {
+        this.setState({ res: 'succ' })
+      }
+      else
+      {
+        this.setState({ res: 'fail' })
+      }
+    }
     return (
     <div>
       <h1>
@@ -32,7 +55,7 @@ class App extends Component {
         onPlay={() => this.setState({ playing: true })}
         playing={this.state.playing}
 
-        url= {video_url[idx]}
+        url= {video_url[idx].url}
         width="0px" // 둘다 0으로바꾸면 백그라운드재생
         height="0px"     
         controls={true}
@@ -50,6 +73,20 @@ class App extends Component {
       <button onClick={() => this.setState({ playing: true })}>재생</button>
       <button onClick={() => this.setState({ playing: false })}>일시정지</button>
       <button onClick={nextVideo}>다음 영상</button>
+      <br></br>
+      <input 
+        type="text"
+        onChange={onTextChange}
+        value = {this.state.text}
+      />
+      <h3>
+        실시간 입력: {this.state.text}
+        <br></br>
+        정답 : {video_url[idx].ans} 
+        <br></br>
+        결과 : {this.state.res}
+              
+      </h3>
     </div>
     )
   }
