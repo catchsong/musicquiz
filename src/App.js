54e98293809ae5e6d47f7ea2287ca0c9b33b730f
우진 <woojin9812071207@gmail.com>
2022-11-25 21:06:45 +0900

    video play api change

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Component } from "react";
-import YoutubeBackground from "react-youtube-background";
+import ReactPlayer from "react-player";
 
 
 class App extends Component {
@@ -8,16 +8,23 @@ class App extends Component {
   };
   render(){
     const vdid = 'K4OltK36GUo'
+    const startTime = 20
+    const endTime = 30
+    const video_url = "https://www.youtube.com/watch?v=K4OltK36GUo"
     return (
     <div>
       <h1>
       MusicQuiz
-        <YoutubeBackground
-        className="yt"
-        videoId= {vdid} // 0trXUYYeNHY
-        playerOptions={{ mute: 0, loop: 1 }}
-        >
-    </YoutubeBackground>
+      <ReactPlayer 
+        className="react-player" 
+        url= {video_url + "#t=${startTime},${endTime}"}
+        width="1000px" // 둘다 0으로바꾸면 백그라운드재생
+        height="1000px" 
+        playing={true}     
+        controls
+        //muted={true}  //크롬에서 muted on이여야 자동재생 ?
+        loops={true}
+      />
       </h1>
 
     </div>
