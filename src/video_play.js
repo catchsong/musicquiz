0ee8af977306c0b133297d7dce011d315f05ea1b
우진 <woojin9812071207@gmail.com>
2022-11-26 08:09:10 +0900

    new func

--- src/video_play.js
+++ src/video_play.js
@@ -1,26 +1,19 @@
 import { Component } from "react";
 import ReactPlayer from "react-player";
-import videoplay from "./video_play"
+
 
 class App extends Component {
-  state = {
-    playing: true,
-    idx: 0
+  constructor (props) {
+  	super(props)
+    this.state = {
+    	playing: true
+    }
   }
   render(){
-    const { idx } = this.state;
-    const video_url = [
-      "https://www.youtube.com/watch?v=K4OltK36GUo",
-      "https://www.youtube.com/watch?v=ZNEuWldWPD4",
-    ]
 
-    const nextVideo = () => {
-      if (idx< video_url.length - 1) {
-        this.setState({ idx: this.state.idx+1 })
-      } else {
-        this.setState({ idx: 0 })
-      }
-    };
+    const startTime = 20
+    const endTime = 23
+    const video_url = "https://www.youtube.com/watch?v=K4OltK36GUo"
     return (
     <div>
       <h1>
@@ -31,10 +24,9 @@ class App extends Component {
         onPause={() => this.setState({ playing: false })}
         onPlay={() => this.setState({ playing: true })}
         playing={this.state.playing}
-
-        url= {video_url[idx]}
-        width="0px" // 둘다 0으로바꾸면 백그라운드재생
-        height="0px"     
+        url= {video_url + `#t=${startTime},${endTime}`}
+        width="500px" // 둘다 0으로바꾸면 백그라운드재생
+        height="500px"     
         controls={true}
         progressInterval={1000}
         pip={true}
@@ -43,13 +35,11 @@ class App extends Component {
 
       />
       상태: {this.state.playing ? 'true' : 'false'}
-      <br></br>
-      idx: {this.state.idx}
 
       </h1>
       <button onClick={() => this.setState({ playing: true })}>재생</button>
       <button onClick={() => this.setState({ playing: false })}>일시정지</button>
-      <button onClick={nextVideo}>다음 영상</button>
+
     </div>
     )
   }
